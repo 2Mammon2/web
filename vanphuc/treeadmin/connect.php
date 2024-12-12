934318d997d34aaa3869ddb00e3014c095053cb2
2Mammon2 <80017085+2Mammon2@users.noreply.github.com>
2024-12-12 15:45:28 +0700

    Update connect.php

--- vanphuc/treeadmin/connect.php
+++ vanphuc/treeadmin/connect.php
@@ -1,5 +1,5 @@
 <?php
-  $link=mysql_pconnect("localhost","root","")
+  $link=mysql_pconnect("websitepentest-f7fmceb3fgfhbedc.southeastasia-01.azurewebsites.net","root","")
   or die("Could not connect to MySQL Database");
   mysql_select_db("vanphuc",$link);
 ?>
